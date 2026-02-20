d534c39edf5e57d23a2e5e136bbdf732d42d72b1
DenisSeleznev <generalpokrichkin@yandex.ru>
2026-02-20 21:42:55 +0300

    Moved the layout to a React component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,32 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { MyComponents } from './MyComponents';
+// import { useState } from 'react';
+import style from './App.module.css';
+// import { MyComponents } from './MyComponents';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount(count => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<MyComponents />
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
+		<div className={style.app}>
+			<h1 className={style.pageHeading}>Ввод значения</h1>
+			<p className={style.noMarginText}>
+				Текущее значение <code>value</code>: "
+				<output className={style.currentValue}></output>"
 			</p>
-		</>
+			<div className={style.error}>
+				Введенное значение должно содержать минимум 3 символа
+			</div>
+			<div className={style.buttonsContainer}>
+				<button className={style.button}>Ввести новое</button>
+				<button className={style.button} disabled>
+					Добавить в список
+				</button>
+			</div>
+			<div className={style.listContainer}>
+				<h2 className={style.listHeading}>Список:</h2>
+				<p className={style.noMarginText}>Нет добавленных элементов</p>
+				<ul className={style.list}>
+					<li className={style.listItem}>Первый элемент</li>
+				</ul>
+			</div>
+		</div>
 	);
 }
 
